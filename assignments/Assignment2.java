82cfe6eb6dda4fe1bc72241b75cb80e0f947eed7
V JEEVAN VRS <vjeevansai27@gmail.com>
2026-09-04 22:57:48 +0530

    Add WordReversalEncoder to reverse words in a sentence

---
+++ assignments/Assignment2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class WordReversalEncoder {
+
+    static String reverseEachWord(String sentence) {
+
+        String[] words = sentence.split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+
+            StringBuilder reversed = new StringBuilder(words[i]);
+            reversed.reverse();
+
+            result.append(reversed);
+
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String sentence = sc.nextLine();
+
+        System.out.println(reverseEachWord(sentence));
+    }
+}
