b863da18aa44eca9c383f80f1e314b958e4aef83
V JEEVAN VRS <vjeevansai27@gmail.com>
2026-09-04 22:21:06 +0530

    Create Assignment1.java

---
+++ assignments/Assignment1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void checkVotingEligibility(int age) {
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age = sc.nextInt();
+
+        checkVotingEligibility(age);
+    }
+}
